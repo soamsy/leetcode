1c9be09358ad9ee4018371adcf11ef8ff6f108c6
Soamsy <thsoams@gmail.com>
2022-01-20 18:23:21 -0700

    Nitpick: naming

--- src/1094_car_pooling.py
+++ src/1094_car_pooling.py
@@ -5,8 +5,8 @@ def carPooling(trips: list[list[int]], capacity: int) -> bool:
         spots[trip[2]] -= trip[0]
     
     needed = 0
-    for i in spots:
-        needed += i
+    for spot in spots:
+        needed += spot
         if needed > capacity:
             return False
     return True
