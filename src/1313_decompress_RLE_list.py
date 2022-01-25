f03b056c4df3e10e035d8ab5bc4ed6bfb9fcaa92
Soamsy <thsoams@gmail.com>
2022-01-24 19:01:53 -0700

    1313

---
+++ src/1313_decompress_RLE_list.py
@@ -0,0 +1,3 @@
+def decompressRLElist(nums: list[int]) -> list[int]:
+    chunks = [nums[i:i+2] for i in range(0, len(nums), 2)]
+    return itertools.chain(*[itertools.repeat(val, freq) for freq, val in chunks])
