f266cf40d8750a5b0663747ec9a1c18764403b87
Soamsy <thsoams@gmail.com>
2022-01-25 02:21:08 -0700

    941

---
+++ src/0941_valid_mountain_array.py
@@ -0,0 +1,22 @@
+def validMountainArray(arr: list[int]) -> bool:
+    if len(arr) < 3:
+        return False
+    
+    i = 1
+    while i < len(arr):
+        if arr[i-1] < arr[i]:
+            i += 1
+        elif arr[i-1] == arr[i]:
+            return False
+        else:
+            break
+    
+    if i == len(arr) or i == 1:
+        return False
+    
+    while i < len(arr):
+        if arr[i-1] > arr[i]:
+            i += 1
+        else:
+            return False
+    return True
