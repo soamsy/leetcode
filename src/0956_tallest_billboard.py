1a57f02c79e8c91d7cadd02ff1d6686d80843029
Soamsy <thsoams@gmail.com>
2022-01-30 11:01:41 -0700

    956

---
+++ src/0956_tallest_billboard.py
@@ -0,0 +1,36 @@
+from functools import cache
+def tallestBillboard(rods: list[int]) -> int:
+    n = len(rods)
+    max_height = sum(rods) // 2
+    
+    @cache
+    def fn(target, i):
+        if abs(target) > max_height:
+            return None
+        
+        if i >= n:
+            if target == 0:
+                return 0
+            else:
+                return None
+            
+        left = fn(target + rods[i], i+1)
+        if left is not None:
+            left += rods[i]
+            
+        right = fn(target - rods[i], i+1)
+        if right is not None:
+            right += rods[i]
+        
+        empty = fn(target, i+1)
+        
+        validPaths = [total for total in [left, right, empty] if total is not None]
+        
+        if validPaths:
+            return max(validPaths)
+        else:
+            return None
+        
+        
+    result = fn(0, 0)
+    return result // 2 if result is not None else 0
