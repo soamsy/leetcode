3a4da182b7469f1a8c3281a9197e80b3e9e77a94
Soamsy <thsoams@gmail.com>
2022-02-04 01:52:28 -0700

    532

---
+++ src/0532_find_pairs.py
@@ -0,0 +1,13 @@
+import collections
+def findPairs(nums: list[int], k: int) -> int:
+    if k == 0:
+        return sum([1 for k, c in collections.Counter(nums).items() if c > 1])
+    
+    nums = list(sorted(set(nums)))
+    indexes = { c: i for i, c in enumerate(nums)}
+    total = 0
+    for i, num in enumerate(nums):
+        if num + k in indexes:
+            total += 1
+        
+    return total
