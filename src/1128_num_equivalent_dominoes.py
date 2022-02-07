d3b7bf96ff89a022e11b3abbcadc04e1a4447471
Soamsy <thsoams@gmail.com>
2022-02-06 17:23:53 -0700

    1128

--- src/1128_num_equivalent_dominoes.py
+++ src/1128_num_equivalent_dominoes.py
@@ -1,3 +1,6 @@
+import collections
+import math
+
 def numEquivDominoPairs(dominoes: list[list[int]]) -> int:
     count = collections.Counter([tuple(sorted(d)) for d in dominoes])
     return sum([math.comb(v, 2) if v > 1 else 0 for v in count.values()])
