4edb33aac8fab24708a031ed2356a2e4ce0e7ed5
Soamsy <thsoams@gmail.com>
2022-02-06 17:25:45 -0700

    Remove unnecessary ternary

--- src/1128_num_equivalent_dominoes.py
+++ src/1128_num_equivalent_dominoes.py
@@ -3,4 +3,4 @@ import math
 
 def numEquivDominoPairs(dominoes: list[list[int]]) -> int:
     count = collections.Counter([tuple(sorted(d)) for d in dominoes])
-    return sum([math.comb(v, 2) if v > 1 else 0 for v in count.values()])
+    return sum([math.comb(v, 2) for v in count.values()])
