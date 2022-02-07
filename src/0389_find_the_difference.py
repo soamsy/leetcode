88d3401f6995d6869bb382567226df98d32534a5
Soamsy <thsoams@gmail.com>
2022-02-06 19:52:12 -0700

    389

---
+++ src/0389_find_the_difference.py
@@ -0,0 +1,10 @@
+def findTheDifference(s: str, t: str) -> str:
+    count = collections.Counter(s)
+    for c in t:
+        if c in count:
+            count[c] -= 1
+            if count[c] == 0:
+                del count[c]
+        else:
+            return c
+    return next(count.keys())
