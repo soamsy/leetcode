7e1f5aee600c2282c35e87bb8df35e944d19348f
Soamsy <thsoams@gmail.com>
2022-02-07 23:05:47 -0700

    Fix types

--- src/0412_fizzbuzz.py
+++ src/0412_fizzbuzz.py
@@ -1,2 +1,2 @@
-def fizzBuzz(self, n: int) -> List[str]:
+def fizzBuzz(self, n: int) -> list[str]:
     return [f"{'Fizz' if i % 3 == 0 else ''}{'Buzz' if i % 5 == 0 else ''}" or str(i) for i in range(1, n + 1)]
