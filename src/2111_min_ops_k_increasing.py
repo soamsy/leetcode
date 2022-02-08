104af1bfb38c2bd080c99b6211a03be2bca50741
Soamsy <thsoams@gmail.com>
2022-02-07 20:56:59 -0700

    2111

---
+++ src/2111_min_ops_k_increasing.py
@@ -0,0 +1,17 @@
+def kIncreasing(arr: list[int], k: int) -> int:
+    def lengthOfLIS(nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] <= x:
+                sub.append(x)
+            else:
+                idx = bisect_right(sub, x)
+                sub[idx] = x
+        return len(sub)
+                
+    chunks = [arr[i:i+k] for i in range(0, len(arr), k)]
+    result = 0
+    for seq in itertools.zip_longest(*chunks, fillvalue=None):
+        seq = list(filter(lambda x: x is not None, seq))
+        result += len(seq) - lengthOfLIS(seq)
+    return result
