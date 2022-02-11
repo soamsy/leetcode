244a93dcd182b53605ce2b112b91f2277b18c687
Soamsy <thsoams@gmail.com>
2022-02-11 00:07:30 -0700

    Remove unused variables

--- src/0056_merge_intervals.py
+++ src/0056_merge_intervals.py
@@ -2,8 +2,6 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
     intervals.sort(key=lambda i: i[0])
     
     r = []
-    start = intervals[0][0]
-    end = intervals[0][1]
     for i in intervals:
         if r and r[-1][1] >= i[0]:
             r[-1][1] = max(r[-1][1], i[1])
